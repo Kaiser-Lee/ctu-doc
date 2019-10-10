4e527c87fddf645af65fd827863d4483cf060fcc
lufangbu <905632226@qq.com>
2019-10-10 13:51:17 +0800

    Initial commit

---
+++ src/main/java/com/ctu/doc/DocApplication.java
@@ -0,0 +1,21 @@
+package com.ctu.doc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 云图文档中心管理系统启动类
+ * User: lufangbu
+ * Date: 2019/10/10
+ * Time: 11:53
+ * version: 1.0.0
+ */
+@SpringBootApplication
+public class DocApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(DocApplication.class, args);
+        System.out.println("启动成功");
+    }
+
+}
---
+++ src/main/java/com/ctu/doc/user/service/T.java
@@ -0,0 +1,11 @@
+package com.ctu.doc.user.service;
+
+/**
+ * Created with Intellil IDEA
+ * User: lufangbu
+ * Date: 2019/10/10
+ * Time: 11:52
+ * version: 1.0.0
+ */
+public class T {
+}
